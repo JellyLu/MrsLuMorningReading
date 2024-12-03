252f30100f82594627a7efce0babcf3d217443ee
陆杨洁 <luyangjie@baicizhan.com>
2024-12-03 21:12:32 +0800

    add script deploy for github page

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,17 @@ import { defineConfig } from 'vite';
 import Paths from "vite-tsconfig-paths";
 import React from "@vitejs/plugin-react";
 import legacy from "@vitejs/plugin-legacy";
-
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    sourcemap: true,
-  },
-  plugins: [
-    React(),
-    legacy({
-      targets: [">0.2%", "not dead", "not op_mini all"],
-    }),
-    Paths(),
+    build: {
+        sourcemap: true,
+    },
+    plugins: [
+        React(),
+        legacy({
+            targets: [">0.2%", "not dead", "not op_mini all"],
+        }),
+        Paths(),
     ],
-  base: '/MrsLuMorningReading'
-})
+    base: '/MrsLuMorningReading'
+});
